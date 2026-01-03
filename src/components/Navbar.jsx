ff3eb8ebcfb0e1b1b8669ebd83f266e20b5f688e
omkarchillal <omkarchillal99@gmail.com>
2026-01-04 04:02:39 +0530

    updated experience info

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Omkar &nbsp;
-            <span className='sm:block hidden'> | FullStack Developer</span>
+            <span className='sm:block hidden'> |&nbsp; FullStack Developer</span>
           </p>
         </Link>
 
